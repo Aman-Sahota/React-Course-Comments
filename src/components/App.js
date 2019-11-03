184388b3dd0bcf1daacf5f8edcc314db62aabdd2
aman sahota <amansahota1996@gmail.com>
2019-11-03 16:17:17 +0530

    Added CommentDetail, faker and semantic-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import faker from "faker";
+
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
-  return <div>Comments</div>;
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Alex"
+        timeAgo="today at 4:45 pm"
+        content="Nice Blog Post"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="today at 6:00 pm"
+        content="I like the subject"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Sam"
+        timeAgo="today at 10:00 pm"
+        content="I like the writing"
+        avatar={faker.image.avatar()}
+      />
+    </div>
+  );
 };
 
 export default App;
